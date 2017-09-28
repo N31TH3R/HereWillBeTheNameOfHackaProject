0f5e4e24e4ea369af35b290d1bcdd17e34b93a91
N31TH3R <nikita.badanin2010@yandex.ru>
2017-09-28 12:18:15 +0300

    Сущности CS

---
+++ Src/OLE/OLE/Entities/Placemark.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OLE.Entities
+{
+    public class Placemark
+    {
+    }
+
+    public struct Coordinates
+    {
+        public float Latitude { get; set; }
+        public float Altitude { get; set; }
+    }
+}
