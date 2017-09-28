c2c536c6a0c4dc1e066d6c665e103c032c6abd6d
N31TH3R <nikita.badanin2010@yandex.ru>
2017-09-28 14:10:57 +0300

    Убрал лишнюю страницу, откорректировал размеры карты(пока что статика), добавил клики.

--- Src/OLE/OLE/wwwroot/js/site.js
+++ Src/OLE/OLE/wwwroot/js/site.js
@@ -1,4 +1,78 @@
-﻿// Write your Javascript code.
-function getPlaceMarks() {
+﻿ymaps.ready(function () {
+    var myMap = new ymaps.Map('map', {
+        center: [55.751574, 37.573856],
+        zoom: 9
+    }, {
+        searchControlProvider: 'yandex#search'
+    });
 
-}
+    // Создаём макет содержимого.
+    MyIconContentLayout = ymaps.templateLayoutFactory.createClass(
+        '<div style="color: #FFFFFF; font-weight: bold;">$[properties.iconContent]</div>'
+    );
+    
+    myPlacemarkWithContent = new ymaps.Placemark([55.661574, 37.573856], {
+        hintContent: 'Собственный значок метки с контентом',
+        balloonContent: 'А эта — новогодняя',
+        iconContent: '12'
+    }, {
+            // Опции.
+            // Необходимо указать данный тип макета.
+            iconLayout: 'default#imageWithContent',
+            // Своё изображение иконки метки.
+            iconImageHref: 'images/AppInstalled.gif',
+            // Размеры метки.
+            iconImageSize: [48, 48],
+            // Смещение левого верхнего угла иконки относительно
+            // её "ножки" (точки привязки).
+            iconImageOffset: [-24, -24],
+            // Смещение слоя с содержимым относительно слоя с картинкой.
+            iconContentOffset: [15, 15],
+            // Макет содержимого.
+            iconContentLayout: MyIconContentLayout
+        });
+    //@for(int i = 0; i < 5; i++)
+    //    {
+    //    myMap.geoObjects
+    //        .add(myPlacemarkWithContent);
+    //    }
+    myMap.geoObjects
+        .add(myPlacemarkWithContent);
+
+    // Обработка события, возникающего при щелчке
+    // левой кнопкой мыши в любой точке карты.
+    // При возникновении такого события откроем балун.
+    myMap.events.add('click', function (e) {
+        if (!myMap.balloon.isOpen()) {
+            var coords = e.get('coords');
+            myMap.balloon.open(coords, {
+                contentHeader: 'Событие!',
+                contentBody: '<p>Кто-то щелкнул по карте.</p>' +
+                '<p>Координаты щелчка: ' + [
+                    coords[0].toPrecision(6),
+                    coords[1].toPrecision(6)
+                ].join(', ') + '</p>',
+                contentFooter: '<sup>Щелкните еще раз</sup>'
+            });
+            myPlacemarkWithContent.coords = coords;
+            myMap.geoObjects
+                .add(myPlacemarkWithContent);
+        }
+        else {
+            myMap.balloon.close();
+        }
+    });
+
+    // Обработка события, возникающего при щелчке
+    // правой кнопки мыши в любой точке карты.
+    // При возникновении такого события покажем всплывающую подсказку
+    // в точке щелчка.
+    myMap.events.add('contextmenu', function (e) {
+        myMap.hint.open(e.get('coords'), 'Кто-то щелкнул правой кнопкой');
+    });
+
+    // Скрываем хинт при открытии балуна.
+    myMap.events.add('balloonopen', function (e) {
+        myMap.hint.close();
+    });
+});
